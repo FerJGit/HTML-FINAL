3ccb3235897cfe81989d295cdc3893cacdc1221f
fernando <fgjaleh@gmail.com>
2023-05-21 22:24:47 -0300

    Agrego menu hamburguesa en 768

--- js/codigo.js
+++ js/codigo.js
@@ -1,10 +1,19 @@
-// ----- Armo el HEADER para todas las páginas -----
+// ----- Armo el HEADER para todas las páginas, incluye btn y menu hamburguesa para máximo 768px -----
 var elem = document.getElementById("idheader");
 elem.innerHTML = `
   <div class="header">
     <div id="logo">
         <a href="index.html"><img src="img/logo.png" alt="logo" width="220vw"></a>
     </div>
+    <div class="btn-hamb">
+      <div class="menu-toggle">
+        <div class="menu-icon">
+          <div class="line line-1"></div>
+          <div class="line line-2"></div>
+          <div class="line line-3"></div>
+        </div>
+      </div>
+    </div>    
     <nav>
         <ul>
         <li><a href="index.html">Inicio</a></li>
@@ -15,6 +24,21 @@ elem.innerHTML = `
     </nav>
   </div>
 `
+
+// ----- Para menú hamburguesa activo/no activo -----------
+const menuToggle = document.querySelector('.menu-toggle');
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('nav');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  menuIcon.classList.toggle('active');
+});
+
+menu.addEventListener('click', () => {
+  menu.classList.remove('active');
+  menuIcon.classList.remove('active');
+});
 
 
 // ----- Armo el FOOTER para todas las páginas -----
@@ -28,17 +52,17 @@ elem.innerHTML = `
 
 // ----- Validación para la pagina de contacto -----
 function validarEmail() {
-    let valor = document.getElementById('InputEmail').value;
-    // Expresión regular para validar el correo electrónico
-    var regex = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*[.]([a-z]{2,3})$/;
-    if (regex.test(valor)){
-      //alert("La dirección de email " + valor + " es correcta.");
-      return true;
-    } else {
-      document.getElementById('InputEmail').focus();
-      alert("La dirección de email " + valor + " es incorrecta.");
-      return false;
-    }
+  let valor = document.getElementById('InputEmail').value;
+  // Expresión regular para validar el correo electrónico
+  var regex = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*[.]([a-z]{2,3})$/;
+  if (regex.test(valor)){
+    //alert("La dirección de email " + valor + " es correcta.");
+    return true;
+  } else {
+    document.getElementById('InputEmail').focus();
+    alert("La dirección de email " + valor + " es incorrecta.");
+    return false;
   }
+}
 
 
